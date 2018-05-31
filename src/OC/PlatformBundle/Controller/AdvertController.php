4ad3ebc3b57b3fa5bf567392abef38441f3419eb
PARHELIA 3 <parhelia3@iMac-de-PARHELIA-2.local>
2018-05-31 09:10:56 +0200

    J2 Cours sur les formulaire

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,9 +5,11 @@
 namespace OC\PlatformBundle\Controller;
 
 use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Form\AdvertType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class AdvertController extends Controller
 {
@@ -78,17 +80,31 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-    $em = $this->getDoctrine()->getManager();
+    // On crée un objet Advert
+    $advert = new Advert();
 
-    // On ne sait toujours pas gérer le formulaire, patience cela vient dans la prochaine partie !
+    // J'ai raccourci cette partie, car c'est plus rapide à écrire !
+    $form = $this->get('form.factory')->create(AdvertType::class, $advert);
+    // ou $form = $this->createForm(AdvertType::class, $advert);
 
-    if ($request->isMethod('POST')) {
-      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+    // Si la requête est en POST
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
 
-      return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($advert);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+        // On redirige vers la page de visualisation de l'annonce nouvellement créée
+        return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
     }
-
-    return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    // À ce stade, le formulaire n'est pas valide car :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+    return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+      'form' => $form->createView(),
+    ));
   }
 
   public function editAction($id, Request $request)
--- var/cache/dev/twig/4f/4f951a5f96438f0604a1195666c6d619f8b0537c8209bc41678cc9708736cd56.php
+++ var/cache/dev/twig/4f/4f951a5f96438f0604a1195666c6d619f8b0537c8209bc41678cc9708736cd56.php
@@ -35,9 +35,90 @@ class __TwigTemplate_d07d5cb6bb6938c8da63fa4b6b76a0b94319d76f39f9021585fa19eff93
 ";
         // line 11
         echo "<div class=\"well\">
-  Ici se trouvera le formulaire.
+  ";
+        // line 12
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new Twig_Error_Runtime('Variable "form" does not exist.', 12, $this->source); })()), 'form');
+        echo "
+  ";
+        // line 13
+        echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock(twig_get_attribute($this->env, $this->source, (isset($context["form"]) || array_key_exists("form", $context) ? $context["form"] : (function () { throw new Twig_Error_Runtime('Variable "form" does not exist.', 13, $this->source); })()), "categories", array()), 'row');
+        echo "
+  <a href=\"#\" id=\"add_category\" class=\"btn btn-default\">Ajouter une catégorie</a>
 </div>
+
 ";
+        // line 19
+        echo "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>
+
+";
+        // line 22
+        echo "<script type=\"text/javascript\">
+  \$(document).ready(function() {
+    // On récupère la balise <div> en question qui contient l'attribut « data-prototype » qui nous intéresse.
+    var \$container = \$('div#oc_platformbundle_advert_categories');
+
+    // On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
+    var index = \$container.find(':input').length;
+
+    // On ajoute un nouveau champ à chaque clic sur le lien d'ajout.
+    \$('#add_category').click(function(e) {
+      addCategory(\$container);
+
+      e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+      return false;
+    });
+
+    // On ajoute un premier champ automatiquement s'il n'en existe pas déjà un (cas d'une nouvelle annonce par exemple).
+    if (index == 0) {
+      addCategory(\$container);
+    } else {
+      // S'il existe déjà des catégories, on ajoute un lien de suppression pour chacune d'entre elles
+      \$container.children('div').each(function() {
+        addDeleteLink(\$(this));
+      });
+    }
+
+    // La fonction qui ajoute un formulaire CategoryType
+    function addCategory(\$container) {
+      // Dans le contenu de l'attribut « data-prototype », on remplace :
+      // - le texte \"__name__label__\" qu'il contient par le label du champ
+      // - le texte \"__name__\" qu'il contient par le numéro du champ
+      var template = \$container.attr('data-prototype')
+        .replace(/__name__label__/g, 'Catégorie n°' + (index+1))
+        .replace(/__name__/g,        index)
+      ;
+
+      // On crée un objet jquery qui contient ce template
+      var \$prototype = \$(template);
+
+      // On ajoute au prototype un lien pour pouvoir supprimer la catégorie
+      addDeleteLink(\$prototype);
+
+      // On ajoute le prototype modifié à la fin de la balise <div>
+      \$container.append(\$prototype);
+
+      // Enfin, on incrémente le compteur pour que le prochain ajout se fasse avec un autre numéro
+      index++;
+    }
+
+    // La fonction qui ajoute un lien de suppression d'une catégorie
+    function addDeleteLink(\$prototype) {
+      // Création du lien
+      var \$deleteLink = \$('<a href=\"#\" class=\"btn btn-danger\">Supprimer</a>');
+
+      // Ajout du lien
+      \$prototype.append(\$deleteLink);
+
+      // Ajout du listener sur le clic du lien pour effectivement supprimer la catégorie
+      \$deleteLink.click(function(e) {
+        \$prototype.remove();
+
+        e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+        return false;
+      });
+    }
+  });
+</script>";
         
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
 
@@ -51,9 +132,14 @@ class __TwigTemplate_d07d5cb6bb6938c8da63fa4b6b76a0b94319d76f39f9021585fa19eff93
         return "OCPlatformBundle:Advert:form.html.twig";
     }
 
+    public function isTraitable()
+    {
+        return false;
+    }
+
     public function getDebugInfo()
     {
-        return array (  37 => 11,  32 => 6,  29 => 2,);
+        return array (  55 => 22,  51 => 19,  44 => 13,  40 => 12,  37 => 11,  32 => 6,  29 => 2,);
     }
 
     public function getSourceContext()
@@ -69,8 +155,82 @@ class __TwigTemplate_d07d5cb6bb6938c8da63fa4b6b76a0b94319d76f39f9021585fa19eff93
 {# On laisse vide la vue pour l'instant, on la comblera plus tard
    lorsqu'on saura afficher un formulaire. #}
 <div class=\"well\">
-  Ici se trouvera le formulaire.
+  {{ form(form) }}
+  {{ form_row(form.categories) }}
+  <a href=\"#\" id=\"add_category\" class=\"btn btn-default\">Ajouter une catégorie</a>
 </div>
-", "OCPlatformBundle:Advert:form.html.twig", "/Users/parhelia3/Dropbox/MAMP/htdocs/Symfony/Symphony/src/OC/PlatformBundle/Resources/views/Advert/form.html.twig");
+
+{# On charge la bibliothèque jQuery. Ici, je la prends depuis le CDN google
+   mais si vous l'avez en local, changez simplement l'adresse. #}
+<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>
+
+{# Voici le script en question : #}
+<script type=\"text/javascript\">
+  \$(document).ready(function() {
+    // On récupère la balise <div> en question qui contient l'attribut « data-prototype » qui nous intéresse.
+    var \$container = \$('div#oc_platformbundle_advert_categories');
+
+    // On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
+    var index = \$container.find(':input').length;
+
+    // On ajoute un nouveau champ à chaque clic sur le lien d'ajout.
+    \$('#add_category').click(function(e) {
+      addCategory(\$container);
+
+      e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+      return false;
+    });
+
+    // On ajoute un premier champ automatiquement s'il n'en existe pas déjà un (cas d'une nouvelle annonce par exemple).
+    if (index == 0) {
+      addCategory(\$container);
+    } else {
+      // S'il existe déjà des catégories, on ajoute un lien de suppression pour chacune d'entre elles
+      \$container.children('div').each(function() {
+        addDeleteLink(\$(this));
+      });
+    }
+
+    // La fonction qui ajoute un formulaire CategoryType
+    function addCategory(\$container) {
+      // Dans le contenu de l'attribut « data-prototype », on remplace :
+      // - le texte \"__name__label__\" qu'il contient par le label du champ
+      // - le texte \"__name__\" qu'il contient par le numéro du champ
+      var template = \$container.attr('data-prototype')
+        .replace(/__name__label__/g, 'Catégorie n°' + (index+1))
+        .replace(/__name__/g,        index)
+      ;
+
+      // On crée un objet jquery qui contient ce template
+      var \$prototype = \$(template);
+
+      // On ajoute au prototype un lien pour pouvoir supprimer la catégorie
+      addDeleteLink(\$prototype);
+
+      // On ajoute le prototype modifié à la fin de la balise <div>
+      \$container.append(\$prototype);
+
+      // Enfin, on incrémente le compteur pour que le prochain ajout se fasse avec un autre numéro
+      index++;
+    }
+
+    // La fonction qui ajoute un lien de suppression d'une catégorie
+    function addDeleteLink(\$prototype) {
+      // Création du lien
+      var \$deleteLink = \$('<a href=\"#\" class=\"btn btn-danger\">Supprimer</a>');
+
+      // Ajout du lien
+      \$prototype.append(\$deleteLink);
+
+      // Ajout du listener sur le clic du lien pour effectivement supprimer la catégorie
+      \$deleteLink.click(function(e) {
+        \$prototype.remove();
+
+        e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+        return false;
+      });
+    }
+  });
+</script>", "OCPlatformBundle:Advert:form.html.twig", "/Users/parhelia3/Dropbox/MAMP/htdocs/Symfony/Symphony/src/OC/PlatformBundle/Resources/views/Advert/form.html.twig");
     }
 }
